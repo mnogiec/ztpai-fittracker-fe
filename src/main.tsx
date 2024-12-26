2888b24f8a11b2b59f18b0a77627f04d003233f4
mnogiec <nogiecmikolaj@gmail.com>
2024-12-26 13:47:57 +0100

    Remove index css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { Routes } from "./Routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
