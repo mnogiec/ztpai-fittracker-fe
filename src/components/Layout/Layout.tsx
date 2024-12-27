8eb08ddb7f49135f5f4c38dffd1118416eb92e19
mnogiec <nogiecmikolaj@gmail.com>
2024-12-27 17:40:05 +0100

    Fix non-main pages padding

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,16 +2,29 @@ import { useState } from "react";
 import { Header } from "../Header/Header";
 import SideMenu from "../SideMenu/SideMenu";
 
-export const Layout = ({ children, isSimpleHeader = false }: { children: React.ReactNode, isSimpleHeader?: boolean }) => {
+export const Layout = ({
+  children,
+  isSimpleHeader = false,
+  isUtils = false,
+}: {
+  children: React.ReactNode;
+  isSimpleHeader?: boolean;
+  isUtils?: boolean;
+}) => {
   const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
 
   return (
     <div>
-      <Header toggleMenu={() => setIsSideMenuOpen(!isSideMenuOpen)} isSimpleHeader={isSimpleHeader} />
+      <Header
+        toggleMenu={() => setIsSideMenuOpen(!isSideMenuOpen)}
+        isSimpleHeader={isSimpleHeader}
+      />
       <SideMenu isOpen={isSideMenuOpen} />
-      <main className="main">
-        {children}
-      </main>
+      {isUtils ? (
+        <div className="utils-main">{children}</div>
+      ) : (
+        <div className="main">{children}</div>
+      )}
     </div>
   );
 };
