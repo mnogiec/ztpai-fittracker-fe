764d7114ffd8cf88d283c1788f10fa8f3326ec54
mnogiec <nogiecmikolaj@gmail.com>
2024-12-31 10:43:39 +0100

    Add debounce to search inputs

--- src/pages/ExercisesBase/ExercisesBasePage.tsx
+++ src/pages/ExercisesBase/ExercisesBasePage.tsx
@@ -1,75 +1,87 @@
-import { useState } from "react";
-import { Layout } from "../../components/Layout/Layout";
-import { Exercise } from "../../models/Exercise";
-import { useQuery } from "@tanstack/react-query";
-import { ExercisesApi, EXERCISES_API_KEYS } from "../../api/ExercisesApi";
-import { UpsertExerciseModal } from "../../components/UpsertExerciseModal/UpsertExerciseModal";
-import { DeleteExerciseModal } from "../../components/DeleteExerciseModal/DeleteExerciseModal";
-import { ExerciseItem } from "../../components/ExerciseItem/ExerciseItem";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { USER_KEY } from "../Login/LoginPage";
-import { User } from "../../models/User";
+import { useState, useCallback } from 'react';
+import { Layout } from '../../components/Layout/Layout';
+import { Exercise } from '../../models/Exercise';
+import { useQuery } from '@tanstack/react-query';
+import { ExercisesApi, EXERCISES_API_KEYS } from '../../api/ExercisesApi';
+import { UpsertExerciseModal } from '../../components/UpsertExerciseModal/UpsertExerciseModal';
+import { DeleteExerciseModal } from '../../components/DeleteExerciseModal/DeleteExerciseModal';
+import { ExerciseItem } from '../../components/ExerciseItem/ExerciseItem';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { USER_KEY } from '../Login/LoginPage';
+import { User } from '../../models/User';
+import debounce from 'lodash/debounce';
 
 export const ExercisesBasePage = () => {
   const [isCreateEditModalOpen, setIsCreateEditModalOpen] = useState(false);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [selectedExercise, setSelectedExercise] = useState<Exercise | null>(
-    null
+    null,
   );
 
-  const user: User | null = JSON.parse(localStorage.getItem(USER_KEY) || "{}");
+  const user: User | null = JSON.parse(localStorage.getItem(USER_KEY) || '{}');
   const canManage = user?.isAdmin;
 
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
+  const [debouncedSearch, setDebouncedSearch] = useState('');
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedSetSearch = useCallback(
+    debounce((value: string) => {
+      setDebouncedSearch(value);
+    }, 500),
+    [],
+  );
 
   const { data: exercisesData } = useQuery({
-    queryKey: [EXERCISES_API_KEYS.GET_ALL_PUBLIC, search],
-    queryFn: () => ExercisesApi.getAllPublic(search),
+    queryKey: [EXERCISES_API_KEYS.GET_ALL_PUBLIC, debouncedSearch],
+    queryFn: () => ExercisesApi.getAllPublic(debouncedSearch),
   });
   const exercises = exercisesData?.data;
 
   return (
     <Layout>
-      <div className="exercises-container">
-        <div className="exercises-topbar">
-          <h1 className="text-4xl font-bold">Exercises Base</h1>
-          <div className="exercises-topbar-right">
+      <div className='exercises-container'>
+        <div className='exercises-topbar'>
+          <h1 className='text-4xl font-bold'>Exercises Base</h1>
+          <div className='exercises-topbar-right'>
             {canManage && (
               <button
-                type="button"
-                className="btn exercises-add-button"
+                type='button'
+                className='btn exercises-add-button'
                 onClick={() => {
                   setSelectedExercise(null);
                   setIsCreateEditModalOpen(true);
-                }}
-              >
-                <span className="exercises-add-button-content">
-                  <span className="exercises-add-text">Add Exercise</span>
-                  <FontAwesomeIcon icon={faPlus} color="white" size="lg" />
+                }}>
+                <span className='exercises-add-button-content'>
+                  <span className='exercises-add-text'>Add Exercise</span>
+                  <FontAwesomeIcon icon={faPlus} color='white' size='lg' />
                 </span>
               </button>
             )}
             <input
-              type="text"
-              placeholder="Search by exercise name"
-              className="exercises-search-input text-input"
+              type='text'
+              placeholder='Search by exercise name'
+              className='exercises-search-input text-input'
               value={search}
-              onChange={(e) => setSearch(e.target.value)}
+              onChange={(e) => {
+                setSearch(e.target.value);
+                debouncedSetSearch(e.target.value);
+              }}
             />
           </div>
         </div>
 
-        <div className="exercises-wrapper">
+        <div className='exercises-wrapper'>
           {!exercises || Object.keys(exercises).length === 0 ? (
-            <p className="exercises-not-found text-gray text-center">
+            <p className='exercises-not-found text-gray text-center'>
               No exercises found
             </p>
           ) : (
             Object.entries(exercises).map(([categoryName, exercisesList]) => (
-              <section key={categoryName} className="exercises-section">
-                <h2 className="text-2xl font-semibold">{categoryName}</h2>
-                <div className="exercises-boxes-wrapper">
+              <section key={categoryName} className='exercises-section'>
+                <h2 className='text-2xl font-semibold'>{categoryName}</h2>
+                <div className='exercises-boxes-wrapper'>
                   {exercisesList?.map((exercise: Exercise) => (
                     <ExerciseItem
                       key={exercise.id}
--- src/pages/PrivateExercises/PrivateExercisesPage.tsx
+++ src/pages/PrivateExercises/PrivateExercisesPage.tsx
@@ -1,68 +1,80 @@
-import { useState } from "react";
-import { Layout } from "../../components/Layout/Layout";
-import { Exercise } from "../../models/Exercise";
-import { useQuery } from "@tanstack/react-query";
-import { ExercisesApi, EXERCISES_API_KEYS } from "../../api/ExercisesApi";
-import { UpsertExerciseModal } from "../../components/UpsertExerciseModal/UpsertExerciseModal";
-import { DeleteExerciseModal } from "../../components/DeleteExerciseModal/DeleteExerciseModal";
-import { ExerciseItem } from "../../components/ExerciseItem/ExerciseItem";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useState, useCallback } from 'react';
+import { Layout } from '../../components/Layout/Layout';
+import { Exercise } from '../../models/Exercise';
+import { useQuery } from '@tanstack/react-query';
+import { ExercisesApi, EXERCISES_API_KEYS } from '../../api/ExercisesApi';
+import { UpsertExerciseModal } from '../../components/UpsertExerciseModal/UpsertExerciseModal';
+import { DeleteExerciseModal } from '../../components/DeleteExerciseModal/DeleteExerciseModal';
+import { ExerciseItem } from '../../components/ExerciseItem/ExerciseItem';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import debounce from 'lodash/debounce';
 
 export const PrivateExercisesPage = () => {
   const [isCreateEditModalOpen, setIsCreateEditModalOpen] = useState(false);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [selectedExercise, setSelectedExercise] = useState<Exercise | null>(
-    null
+    null,
   );
 
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
+  const [debouncedSearch, setDebouncedSearch] = useState('');
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedSetSearch = useCallback(
+    debounce((value: string) => {
+      setDebouncedSearch(value);
+    }, 500),
+    [],
+  );
 
   const { data: exercisesData } = useQuery({
-    queryKey: [EXERCISES_API_KEYS.GET_ALL_PRIVATE, search],
-    queryFn: () => ExercisesApi.getAllPrivate(search),
+    queryKey: [EXERCISES_API_KEYS.GET_ALL_PRIVATE, debouncedSearch],
+    queryFn: () => ExercisesApi.getAllPrivate(debouncedSearch),
   });
   const exercises = exercisesData?.data;
 
   return (
     <Layout>
-      <div className="exercises-container">
-        <div className="exercises-topbar">
-          <h1 className="text-4xl font-bold">Private Exercises</h1>
-          <div className="exercises-topbar-right">
+      <div className='exercises-container'>
+        <div className='exercises-topbar'>
+          <h1 className='text-4xl font-bold'>Private Exercises</h1>
+          <div className='exercises-topbar-right'>
             <button
-              type="button"
-              className="btn exercises-add-button"
+              type='button'
+              className='btn exercises-add-button'
               onClick={() => {
                 setSelectedExercise(null);
                 setIsCreateEditModalOpen(true);
-              }}
-            >
-              <span className="exercises-add-button-content">
-                <span className="exercises-add-text">Add Exercise</span>
-                <FontAwesomeIcon icon={faPlus} color="white" size="lg"  />
+              }}>
+              <span className='exercises-add-button-content'>
+                <span className='exercises-add-text'>Add Exercise</span>
+                <FontAwesomeIcon icon={faPlus} color='white' size='lg' />
               </span>
             </button>
             <input
-              type="text"
-              placeholder="Search by exercise name"
-              className="exercises-search-input text-input"
+              type='text'
+              placeholder='Search by exercise name'
+              className='exercises-search-input text-input'
               value={search}
-              onChange={(e) => setSearch(e.target.value)}
+              onChange={(e) => {
+                setSearch(e.target.value);
+                debouncedSetSearch(e.target.value);
+              }}
             />
           </div>
         </div>
 
-        <div className="exercises-wrapper">
+        <div className='exercises-wrapper'>
           {!exercises || Object.keys(exercises).length === 0 ? (
-            <p className="exercises-not-found text-gray text-center">
+            <p className='exercises-not-found text-gray text-center'>
               No exercises found
             </p>
           ) : (
             Object.entries(exercises).map(([categoryName, exercisesList]) => (
-              <section key={categoryName} className="exercises-section">
-                <h2 className="text-2xl font-semibold">{categoryName}</h2>
-                <div className="exercises-boxes-wrapper">
+              <section key={categoryName} className='exercises-section'>
+                <h2 className='text-2xl font-semibold'>{categoryName}</h2>
+                <div className='exercises-boxes-wrapper'>
                   {exercisesList?.map((exercise: Exercise) => (
                     <ExerciseItem
                       key={exercise.id}
